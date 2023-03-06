d1e4415177c0413bf6bc8635cd906f8baabfa5d4
landhb <landhb@users.noreply.github.com>
2023-03-06 10:55:12 -0500

    More packaging updates.

--- scripts/package.sh
+++ scripts/package.sh
@@ -3,6 +3,7 @@ set -e
 trap 'previous_command=$this_command; this_command=$BASH_COMMAND' DEBUG
 trap 'echo FAILED COMMAND: $previous_command' EXIT
 
+: ${ARCH=x86}
 : ${KERNEL:=4.19.237}
 BUILD_DIR=/tmp/package-linux-$KERNEL
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
@@ -37,8 +38,8 @@ popd
 
 # Build the kernel image
 pushd $BUILD_DIR/linux-$KERNEL
-make defconfig
-make kvmconfig | make kvm_guest.config
+make ARCH=$ARCH defconfig
+#make ARCH=$ARCH kvmconfig | make ARCH=$ARCH kvm_guest.config
 echo "
 CONFIG_CRYPTO_RSA=y
 
@@ -46,9 +47,12 @@ CONFIG_CRYPTO_RSA=y
 CONFIG_KCOV=y
 
 # Debug info for symbolization.
+CONFIG_KGDB=y
+CONFIG_FRAME_POINTER=y
 CONFIG_DEBUG_INFO=y
 CONFIG_GDB_SCRIPS=y
 CONFIG_DBUG_KERNEL=y
+CONFIG_DEBUG_OBJECTS_RCU_HEAD=y
 
 # Memory bug detector
 CONFIG_KASAN=y
@@ -57,17 +61,18 @@ CONFIG_KASAN_INLINE=y
 # Required for Debian Stretch
 CONFIG_CONFIGFS_FS=y
 CONFIG_SECURITYFS=y" >> .config
-make olddefconfig
-make -j`nproc`
+sort .config | uniq -u >> .config2 && mv .config2 .config
+#make ARCH=$ARCH olddefconfig
+make ARCH=$ARCH -j`nproc`
 cp arch/x86_64/boot/bzImage $BUILD_DIR/bzImage-linux-$KERNEL
 popd
 
 # Package the headers and module information
 pushd $BUILD_DIR
-KCONFIG_CONFIG=.config SRCARCH=x86 objtree=$BUILD_DIR/linux-$KERNEL/ srctree=$BUILD_DIR/linux-$KERNEL/ $SCRIPT_DIR/module_headers_install.sh
+KCONFIG_CONFIG=.config SRCARCH=$ARCH objtree=$BUILD_DIR/linux-$KERNEL/ srctree=$BUILD_DIR/linux-$KERNEL/ $SCRIPT_DIR/module_headers_install.sh
 cd linux-modules-headers
 tar -czvf ../linux-$KERNEL-headers.tar.gz *
 popd
 
 
-echo "Package is in $BUILD_DIR" || true
+echo "Package is in $BUILD_DIR"
